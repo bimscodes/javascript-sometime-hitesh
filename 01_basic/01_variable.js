759d26a49eff5b3b690cabacb38ab464bc1cd5e2
Bimal BK <42174757+bimscodes@users.noreply.github.com>
2023-08-29 14:19:05 +0000

    variable-Commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,24 @@
+const accountId=24;
+let accountName="bimal";
+var accountPassword="123456";
+accountCity="kathamandu";
+
+// accountId=2; not allowed
+accountName="dipa"
+accountPassword="34443"
+accountCity="butwal"
+
+if (condition) {
+    
+} else {
+    
+}
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.log([accountId,accountName,accountPassword,accountCity]);
+console.table([accountId,accountName,accountPassword,accountCity]);
