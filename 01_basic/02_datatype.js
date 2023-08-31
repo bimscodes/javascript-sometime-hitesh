8535c1ef2b97cd46e2de8c2d14e6bf9a5ce67172
Bimal BK <42174757+bimscodes@users.noreply.github.com>
2023-08-31 01:20:26 +0000

    02_datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,4 @@
+ "use strict"; //"use strict"; Defines that JavaScript code should be executed in "strict mode".
+
+myName="bimal"; // // This will cause an error because myName is not declared
+console.log(myName)
